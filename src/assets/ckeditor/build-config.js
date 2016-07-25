4a89041123e8a5330b3a7569b443542bead54a5e
SDKiller <jexy.ru@gmail.com>
2016-07-25 13:25:00 +0300

    Removed leaflet plugin from CKEditor custom build

--- src/assets/ckeditor/build-config.js
+++ src/assets/ckeditor/build-config.js
@@ -1,5 +1,5 @@
 ﻿/**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/99b8dca31e5ac1595470e90a6109624f
+ * (2) http://ckeditor.com/builder/27b36a897cf300d595f8b44012cc95d7
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/99b8dca31e5ac1595470e90a6109624f
+ * (3) http://ckeditor.com/builder/download/27b36a897cf300d595f8b44012cc95d7
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -79,7 +79,6 @@ var CKBUILDER_CONFIG = {
 		'indentlist' : 1,
 		'justify' : 1,
 		'language' : 1,
-		'leaflet' : 1,
 		'link' : 1,
 		'list' : 1,
 		'liststyle' : 1,
